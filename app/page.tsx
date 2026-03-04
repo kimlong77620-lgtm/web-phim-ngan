a1fa04698ed081355571a29f34d80dbc1d10a26a
Zhaodi <kimlong77620@gmail.com>
2026-03-04 23:13:38 +0700

    cap nhat giao dien sap nha zhaodi va phim thuong tong

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const danhSachPhim = [
     title: "Sau Khi Ẩn Hôn Với Thương Tổng, Hình Tượng Lạnh Lùng Của Anh Ấy Sụp Đổ - Bản Full",
     thumb: "https://vz-f76c4946-df1.b-cdn.net/f3a3fd9d-b2bd-4399-a361-6f62ae8ae470/thumbnail_dad53ad2.jpg", // Link ảnh Poster phim
     videoSrc: "https://vz-f76c4946-df1.b-cdn.net/f3a3fd9d-b2bd-4399-a361-6f62ae8ae470/play_720p.mp4",
-    audioSrc: "https://iframe.mediadelivery.net/play/610790/95270cae-75f8-4f2a-91bf-96ad5cf8fb96/play.mp3",
+    audioSrc: "https://vz-f76c4946-df1.b-cdn.net/95270cae-75f8-4f2a-91bf-96ad5cf8fb96/play.mp3",
   },
   {
     id: 2,
@@ -53,7 +53,7 @@ function PhongChieu({ phim, onClose }: { phim: any; onClose: () => void }) {
 
   useEffect(() => {
     if (videoRef.current) {
-      videoRef.current.volume = isThuyetMinh ? 0.2 : 1.0;
+      videoRef.current.volume = isThuyetMinh ? 0.3 : 1.0;
     }
   }, [isThuyetMinh]);
 
